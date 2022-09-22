ed4b4efda70af922cad6350abd07e74ca8cec354
Teddy Ordoñez <teddyonu@hotmail.com>
2022-09-21 22:49:31 -0300

    Added numpy dot product

--- rnn.py
+++ rnn.py
@@ -2,17 +2,28 @@
 # By: Teddy Ordoñez
 # Source: https://www.youtube.com/watch?v=Wo5dMEP_BbI&list=PLQVvvaa0QuDcjD5BAw2DxE6OF2tius3V3
 
+import numpy as np
+
 inputs = [1, 2, 3, 2.5]
 
-weights1 = [0.2, 0.8, -0.5, 1.0]
-weights2 = [0.5, -0.91, 0.26, -0.5]
-weights3 = [-0.26, -0.27, 0.17, 0.87]
+weights = [[0.2, 0.8, -0.5, 1.0],
+           [0.5, -0.91, 0.26, -0.5],
+           [-0.26, -0.27, 0.17, 0.87]]
 
-bias1 = 2
-bias2 = 3
-bias3 = 0.5
+biases = [2, 3, 0.5]
 
-output = [inputs[0]* weights1[0] + inputs[1] * weights1[1] + inputs[2] * weights1[2] + inputs[3] * weights1[3] + bias1,
-          inputs[0]* weights2[0] + inputs[1] * weights2[1] + inputs[2] * weights2[2] + inputs[3] * weights2[3] + bias2,
-          inputs[0]* weights3[0] + inputs[1] * weights3[1] + inputs[2] * weights3[2] + inputs[3] * weights3[3] + bias3]
+output = np.dot(weights, inputs) + biases   # The order matter due to shapes of weights and inputs
 print(output)
+'''
+layer_outputs = []  # Output of current layer (size will change)
+for neuron_weigths, neuron_bias in zip(weights, biases):
+    neuron_output = 0   # Output for a specific neuron   
+    
+    for n_input, weight in zip(inputs, neuron_weigths):
+        neuron_output = n_input * weight    # Multiplying the input times the weight
+    
+    neuron_output += neuron_bias    # Adding the bias of the specific neuron to the output
+    layer_outputs.append(neuron_output) # Appending the result of the calculation to the list of results
+
+print(layer_outputs)
+'''
